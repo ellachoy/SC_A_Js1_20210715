b7344a6b6cfc0ca39b8316370d67548227d344e2
Ella Choy <egal21de@gmail.com>
2021-07-15 13:16:04 +0200

    js

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+// let person=Ella
+// person=Bella
+// console.log(person)
+
+let personA="Ella"
+personA="Bella"
+personA="Villa"
+// personA=7
+console.log(personA)
+
+
+
+let age=20
+age=30
+console.log(age)
+// test
+
+let priceMacbookOhne=1200
+let mehrWertSteuer19=0.19
+let priceMacbookMit=priceMacbookOhne+priceMacbookOhne*mehrWertSteuer19
+
+console.log(priceMacbookMit)
+
+let hero="batman"
+let city="Gotham city"
+let ageBatman=30
+
+let product="Iphone"
+let price=1000
+
+
+
+console.log("the product"+  product +   "cost" +   "$")
