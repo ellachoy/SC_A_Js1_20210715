da2c222e9fe6b48c855caba8799d290c3eda21b8
Ella Choy <egal21de@gmail.com>
2021-07-15 15:10:10 +0200

    Js intro

---
+++ assets/js/lev1_7.js
@@ -0,0 +1,34 @@
+
+
+let helloworld="Hello World ,"
+console.log(helloworld)
+
+ let pi=3.14 
+ console.log(pi)
+
+ let burjdubai = "828m ,"
+ console.log(burjdubai)
+
+ let fullname ="Jan Schmidt, " 
+ console.log(fullname)
+
+ let eifeltower = "324m " 
+ console.log(eifeltower)
+
+ let camelcase = "camelCase, " 
+ console.log(camelcase)
+
+ let kursstatus = true
+ console.log(kursstatus )
+
+
+
+
+
+
+
+
+
+
+
+
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,6 @@
-// let person=Ella
-// person=Bella
-// console.log(person)
-
-let personA="Ella"
-personA="Bella"
-personA="Villa"
-// personA=7
-console.log(personA)
+function myFunction() {
+    document.getElementById("test").style.color = "red";
+  }
 
 
 
-let age=20
-age=30
-console.log(age)
-// test
-
-let priceMacbookOhne=1200
-let mehrWertSteuer19=0.19
-let priceMacbookMit=priceMacbookOhne+priceMacbookOhne*mehrWertSteuer19
-
-console.log(priceMacbookMit)
-
-let hero="batman"
-let city="Gotham city"
-let ageBatman=30
-
-let product="Iphone"
-let price=1000
-
-
-
-console.log("the product"+  product +   "cost" +   "$")
